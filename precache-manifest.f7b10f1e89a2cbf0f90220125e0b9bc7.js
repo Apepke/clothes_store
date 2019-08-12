1127f2ffab5f9518a47f047308ff18daf19787a2
Alex <alexpepke@Alexs-MacBook-Pro.local>
2019-08-12 15:36:12 -0500

    Updates

---
+++ precache-manifest.f7b10f1e89a2cbf0f90220125e0b9bc7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52cbb43ec230fabe1e970aa0e07a63bf",
+    "url": "/clothes_store/index.html"
+  },
+  {
+    "revision": "3d406a565d3bd4962def",
+    "url": "/clothes_store/static/css/2.495c32cc.chunk.css"
+  },
+  {
+    "revision": "9012459118d31fbdf11f",
+    "url": "/clothes_store/static/css/main.114fdb04.chunk.css"
+  },
+  {
+    "revision": "3d406a565d3bd4962def",
+    "url": "/clothes_store/static/js/2.707df102.chunk.js"
+  },
+  {
+    "revision": "9012459118d31fbdf11f",
+    "url": "/clothes_store/static/js/main.29ae8a23.chunk.js"
+  },
+  {
+    "revision": "4c8143fcc67b8db34d21",
+    "url": "/clothes_store/static/js/runtime~main.c1dd8a28.js"
+  },
+  {
+    "revision": "cba08df63254fbb3cb3f757731e987a6",
+    "url": "/clothes_store/static/media/logo.cba08df6.svg"
+  }
+]);
